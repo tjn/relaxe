a1211fbcf743da0c21ff871b01e6758f3baf897b
Topi Nieminen <topi.nieminen@gmail.com>
2010-02-23 15:22:38 +0200

    Rename EntityMap to ReferenceMap

--- src/fi/tnie/db/DefaultEntity.java
+++ src/fi/tnie/db/DefaultEntity.java
@@ -8,9 +8,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
+import fi.tnie.db.exec.QueryProcessorAdapter;
 import fi.tnie.db.expr.ColumnName;
 import fi.tnie.db.expr.ElementList;
 import fi.tnie.db.expr.InsertStatement;
@@ -18,6 +23,7 @@ import fi.tnie.db.expr.ValueParameter;
 import fi.tnie.db.expr.ValueRow;
 import fi.tnie.db.meta.BaseTable;
 import fi.tnie.db.meta.Column;
+import fi.tnie.db.meta.ForeignKey;
 import fi.tnie.db.meta.impl.ColumnMap;
 
 public abstract class DefaultEntity<
@@ -29,14 +35,39 @@ public abstract class DefaultEntity<
 	extends AbstractEntity<A, R, Q, E> {
 	
 	private EnumMap<A, Object> values;
-	private EntityMap<R, Entity<?, ?, ?>> refs;
+	private ReferenceMap<R, Entity<?, ?, ?>> refs;
+	
+	private static Logger logger = Logger.getLogger(DefaultEntity.class);
 	
 	protected DefaultEntity() {
 		super();		
 	}
 
 	public Object get(A a) {
+		if (a == null) {
+			throw new NullPointerException("'a' must not be null");
+		}
+		
 		return attrs().get(a);		
+	};
+	
+	/**
+	 * Returns a value of the attribute correpponding the column <code>c</code>  
+	 * @param r
+	 * @return
+	 */
+	public Object get(Column c) {
+		if (c == null) {
+			throw new NullPointerException("'c' must not be null");
+		}
+		
+		A a = getMetaData().getAttribute(c);
+		
+		if (a == null) {
+			throw new IllegalArgumentException("no attribute corresponding the column: " + c);
+		}
+		
+		return get(a);		
 	};
 	
 	public void set(A a, Object value) {
@@ -50,17 +81,9 @@ public abstract class DefaultEntity<
 		return values;
 	}
 	
-//	private EnumMap<R, Entity<?, ?, ?>> refs() {
-//		if (refs == null) {
-//			refs = new EnumMap<R, Entity<?,?,?>>(getMetaData().getRelationshipNameType()); 
-//		}
-//		
-//		return refs;
-//	}
-
-	private EntityMap<R, Entity<?,?,?>> refs() {
+	private ReferenceMap<R, Entity<?,?,?>> refs() {
 		if (refs == null) {
-			refs = new EntityMap<R, Entity<?,?,?>>(getMetaData().getRelationshipNameType()); 
+			refs = new ReferenceMap<R, Entity<?,?,?>>(getMetaData().getRelationshipNameType()); 
 		}
 		
 		return refs;
@@ -76,63 +99,124 @@ public abstract class DefaultEntity<
 	
 
 	// TODO EntityDiff<E> & MergeStrategy ms  
-	public void store() {
-			
+	public void store() {			
 		
 	}
 	
 	public void insert(Connection c) 
 		throws SQLException {		
  
-	InsertStatement q = createInsertQuery();		
-	String qs = q.generate();		
-	final PreparedStatement ps = c.prepareStatement(qs, Statement.RETURN_GENERATED_KEYS);					
-	q.traverse(null, new ParameterAssignment(ps));
+		InsertStatement q = createInsertQuery();
+		String qs = q.generate();		
 		
-	int ins = ps.executeUpdate();
-	
-//	logger().debug("inserted: " + ins);
-	
-	ResultSet rs = ps.getGeneratedKeys();
-	
-	try {
+		logger().debug("JDBC-driver :" + c.getMetaData().getDriverName());
+		logger().debug("JDBC-driver-maj:" + c.getMetaData().getDriverMajorVersion());
+		logger().debug("JDBC-driver-min:" + c.getMetaData().getDriverMinorVersion());
+		logger().debug("JDBC-support-get-gen:" + c.getMetaData().supportsGetGeneratedKeys());
+		
+		final PreparedStatement ps = c.prepareStatement(qs, Statement.RETURN_GENERATED_KEYS);
+		q.traverse(null, new ParameterAssignment(ps));
+		
+		logger().debug("q: " + qs);
+		int ins = ps.executeUpdate();		
+		logger().debug("inserted: " + ins);
+		
+		ResultSet rs = ps.getGeneratedKeys();
+		
+		try {
+//			logger().debug(buf.toString());
+			
+			if (rs.next()) {
+//				buf.setLength(0);
+				
+					int cc = rs.getMetaData().getColumnCount();
+	//			StringBuffer buf = new StringBuffer();			
+				
+				EntityMetaData<A, R, ?, E> em = getMetaData();
+				BaseTable table = em.getBaseTable();
+				ColumnMap cm = table.columnMap();
+				List<A> keys = new ArrayList<A>();
+							
+				for (int i = 1; i <= cc; i++) {
+					String name = rs.getMetaData().getColumnLabel(i);
+					Column col = cm.get(name);
+					
+					if (col == null) {
+						throw new NullPointerException("Can not find column " + name + " in table: " + table.getQualifiedName());
+					} 
+					
+					A attr = em.getAttribute(col);				
+					keys.add(attr);				
+					
+	//				buf.append(name);
+	//				buf.append("\t");
+				}			
 								
-		// TODO: read generated keys:									
-//		RowFactory<K, E> ef = getFactory();
-//		EnumMap<K, Integer> keys = ef.columns(rs.getMetaData());					
-//		
-//		if (rs.next()) {
-//			ef.copy(keys, rs, this);				
-//		}
-	}
-	finally {
-		rs.close();
-	}
-}
-
+				for (int i = 1; i <= cc; i++) {					
+					A attr = keys.get(i - 1);
+					
+					if (attr != null) {
+						Object value = rs.getObject(i);
+						set(attr, value);
+					}
+															
+//					String col = rs.getString(i);
+//					buf.append(col);
+//					buf.append("\t");
+				}
+				
+//				logger().debug(buf.toString());				
+			}
+		}
+		finally {
+			rs = QueryHelper.doClose(rs);			
+		}
+	}	
 
 	public InsertStatement createInsertQuery() {
 	
 		ValueRow newRow = new ValueRow();
+		 
+		final EntityMetaData<A, R, ?, E> meta = getMetaData();				
+		BaseTable t = meta.getBaseTable();
 				
-		BaseTable t = getMetaData().getBaseTable();
-				
-//		getMetaData().getForeignKey(r)
-		
-//		getMetaData().relationships()
-		
-//		ColumnMap cm = t.columnMap();
 		ElementList<ColumnName> names = new ElementList<ColumnName>();
-				
-//		for (Map.Entry<C, ?> e : values.entrySet()) {			
-//			C column = e.getKey();			
-//			Column col = cm.get(column.identifier());
-//			names.add(col.getColumnName());
-//			ValueParameter p = new ValueParameter(col, e.getValue());
-//			newRow.add(p);
-//		}
+		
+		for (Map.Entry<A, Object> e : attrs().entrySet()) {			
+			Column col = meta.getColumn(e.getKey());
+			ValueParameter p = new ValueParameter(col, e.getValue());
+			newRow.add(p);
+			names.add(col.getColumnName());
+		}
+		
+		
+		for (Map.Entry<R, Entity<?, ?, ?>> e : refs().entrySet()) {			
+			ForeignKey fk = meta.getForeignKey(e.getKey());			
+			Entity<?, ?, ?> ref = e.getValue();
+			
+			if (ref == null) {
+				for (Column	c : fk.columns().keySet()) {
+					ValueParameter p = new ValueParameter(c, null);
+					newRow.add(p);
+					names.add(c.getColumnName());
+				}
+			}
+			else {
+				for (Map.Entry<Column, Column> ce : fk.columns().entrySet()) {
+					Column fc = ce.getValue();
+					Object o = ref.get(fc);
+					ValueParameter p = new ValueParameter(ce.getKey(), o);										
+					newRow.add(p);
+					names.add(ce.getKey().getColumnName());
+				}
+			}
+		}	
 				
 		return new InsertStatement(t, names, newRow);
 	}
-
+	
+	
+	public static Logger logger() {
+		return DefaultEntity.logger;
+	}
 }
--- src/fi/tnie/db/ReferenceMap.java
+++ src/fi/tnie/db/ReferenceMap.java
@@ -6,20 +6,21 @@ package fi.tnie.db;
 import java.util.EnumMap;
 import java.util.Map;
 
-public class EntityMap<
+@SuppressWarnings("serial")
+public class ReferenceMap<
 	K extends Enum<K>, 
 	V extends Entity<?, ?, ?>>
 	extends EnumMap<K, V> {
 
-	public EntityMap(Class<K> arg0) {
+	public ReferenceMap(Class<K> arg0) {
 		super(arg0);
 	}
 
-	public EntityMap(EnumMap<K, ? extends V> m) {
+	public ReferenceMap(EnumMap<K, ? extends V> m) {
 		super(m);		
 	}
 
-	public EntityMap(Map<K, ? extends V> m) {
+	public ReferenceMap(Map<K, ? extends V> m) {
 		super(m);	
 	}
 }
