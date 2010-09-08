03826b622cbcdff7dc51e0201082a857b46cbfe7
Topi Nieminen <topi.nieminen@gmail.com>
2010-09-08 14:12:14 +0300

    Add bas class for Holder's

---
+++ src/fi/tnie/db/Holder.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2009-2013 Topi Nieminen
+ */
+package fi.tnie.db;
+
+import java.io.Serializable;
+
+/**
+ * Typed, immutable value holder.
+ * 
+ * @author tnie
+ */
+public abstract class Holder
+	implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2127849751940907829L;
+	
+}
