3f38afafdc29679dc0e6a6fff198d7311edd97e5
Topi Nieminen <topi.nieminen@gmail.com>
2010-09-08 13:35:51 +0300

    Add script to generate source code

---
+++ script/generate.bat
@@ -0,0 +1,25 @@
+@echo off
+SET ROOT=%~dp0..
+SET JARDIR=%ROOT%\lib
+
+SET GENSRC=%ROOT%\gen\src
+
+MKDIR %ROOT%\gen 2> NULL
+MKDIR %GENSRC% 2> NULL
+
+SET JDBC_CONFIG=%ROOT%\pg.properties
+SET JDBC_URL=jdbc:postgresql:test
+SET ENV=fi.tnie.db.meta.impl.pg.PGEnvironment
+SET ROOT_PACKAGE=fi.tnie.db.build
+
+REM echo %JARDIR%
+
+REM Build java classpath:
+SET META_GEN_CP=%ROOT%\classes
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\postgresql-8.4-701.jdbc3.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\util.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\log4j.jar
+
+REM read to go:
+java -classpath %META_GEN_CP% fi.tnie.db.build.Builder -g %GENSRC% -j %JDBC_CONFIG% -e %ENV% --root-package %ROOT_PACKAGE% -u %JDBC_URL%
+@echo on
