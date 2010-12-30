55937f503a4097a4ccfae89c48d2a8d3ab6e8702
Topi Nieminen <topi.nieminen@gmail.com>
2010-12-30 16:10:31 +0200

    Add MutableStringModel

---
+++ src/fi/tnie/db/model/MutableStringModel.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2009-2013 Topi Nieminen
+ */
+package fi.tnie.db.model;
+
+public class MutableStringModel
+	extends DefaultMutableValueModel<String> {
+
+	public MutableStringModel() {
+		super();
+	}
+
+	public MutableStringModel(String value) {
+		super(value);	
+	}		
+}
