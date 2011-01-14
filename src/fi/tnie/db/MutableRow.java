ba11b79fbe2b43c57e2746eb0ad2c35362adaef0
Topi Nieminen <topi.nieminen@gmail.com>
2011-01-14 22:26:05 +0200

    Remove sketch -like classes from db -package

--- src/fi/tnie/db/MutableRow.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2009-2013 Topi Nieminen
- */
-package fi.tnie.db;
-
-public interface MutableRow
-	extends Row {
-	void set(int column, Object value);	
-}
--- src/fi/tnie/db/TableRow.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2009-2013 Topi Nieminen
- */
-package fi.tnie.db;
-
-public interface TableRow {
-	void set(int ordinal, Object value);	
-}
