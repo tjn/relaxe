da0b5de13a34de9f722b816f9b0f55321887c0ea
Topi Nieminen <topi.nieminen@gmail.com>
2011-01-21 18:36:30 +0200

    Add serialVersionUID

--- src/fi/tnie/db/expr/VisitException.java
+++ src/fi/tnie/db/expr/VisitException.java
@@ -5,6 +5,11 @@ package fi.tnie.db.expr;
 
 public class VisitException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2802246489601806383L;
+
 	public VisitException(String message) {
 		super(message);
 	}
