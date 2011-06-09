247eda8435e3663a8275f6bab9c43b683d82d0a9
Topi Nieminen <topi.nieminen@gmail.com>
2011-06-09 16:18:54 +0300

    Add catalog-info batch files

---
+++ script/cat/catalog-info-mysql.bat
@@ -0,0 +1,18 @@
+@echo off
+SET HOME=%~dp0
+SET ROOT=%HOME%..\..
+SET JARDIR=%ROOT%\lib
+
+SET JDBC_CONFIG=%ROOT%\mysql.properties
+SET JDBC_URL=jdbc:mysql://127.0.0.1/mysql
+
+REM Build java classpath:
+SET META_GEN_CP=%HOME%
+SET META_GEN_CP=%META_GEN_CP%;%ROOT%\classes
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\mysql-connector-java-5.1.11-bin.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\util.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\log4j.jar
+
+REM read to go:
+java -classpath %META_GEN_CP% fi.tnie.dbmeta.tools.CatalogInfo -v -j %JDBC_CONFIG% -u %JDBC_URL%
+@echo on
---
+++ script/cat/catalog-info-pg.bat
@@ -0,0 +1,19 @@
+@echo off
+SET HOME=%~dp0
+SET ROOT=%HOME%..\..
+SET JARDIR=%ROOT%\lib
+
+SET JDBC_CONFIG=%ROOT%\pg.properties
+SET JDBC_URL=jdbc:postgresql:test
+SET ENV=fi.tnie.db.env.pg.PGImplementation
+
+REM Build java classpath:
+SET META_GEN_CP=%HOME%
+SET META_GEN_CP=%META_GEN_CP%;%ROOT%\classes
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\postgresql-8.4-701.jdbc3.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\util.jar
+SET META_GEN_CP=%META_GEN_CP%;%JARDIR%\log4j.jar
+
+REM read to go:
+java -classpath %META_GEN_CP% fi.tnie.dbmeta.tools.CatalogInfo -v -j %JDBC_CONFIG% -u %JDBC_URL%
+@echo on
