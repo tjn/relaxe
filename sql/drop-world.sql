6665f5640f151f67e338ac0c77fed23fbad3ac21
Topi Nieminen <topi.nieminen@gmail.com>
2011-10-01 20:15:06 +0300

    Add DDL for test database

---
+++ sql/drop-world.sql
@@ -0,0 +1,6 @@
+
+
+drop table if exists continent cascade
+;
+
+
---
+++ sql/pop-continent.sql
@@ -0,0 +1,11 @@
+
+insert into continent (name) 
+values
+('Asia'),
+('Europe'),
+('North America'),
+('Africa'),
+('Oceania'),
+('Antarctica'),
+('South America')
+;
