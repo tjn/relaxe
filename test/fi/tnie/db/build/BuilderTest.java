9d765b25369d6796b45db38515712bcc33e1a1db
Topi Nieminen <topi.nieminen@gmail.com>
2011-10-03 14:45:25 +0300

    BuilderTest: Remove invalid import

--- test/fi/tnie/db/build/BuilderTest.java
+++ test/fi/tnie/db/build/BuilderTest.java
@@ -17,7 +17,6 @@ import java.util.Set;
 
 import org.apache.log4j.Logger;
 
-import fi.tnie.db.DBMetaUnitTest;
 import fi.tnie.db.DefaultEntityContext;
 import fi.tnie.db.DefaultTableMapper;
 import fi.tnie.db.ent.EntityContext;
