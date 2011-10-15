1411c9a4464f5348f99a5d146bf92b2dbdcc9daf
Topi Nieminen <topi.nieminen@gmail.com>
2011-10-15 08:56:59 +0300

    Let BooleanNotModel implement BooleanModel

--- src/fi/tnie/db/model/BooleanNotModel.java
+++ src/fi/tnie/db/model/BooleanNotModel.java
@@ -7,7 +7,8 @@
 package fi.tnie.db.model;
 
 public class BooleanNotModel
-	extends AbstractTransformationModel<Boolean, Boolean> {
+	extends AbstractTransformationModel<Boolean, Boolean>
+	implements BooleanModel {
 
 	public BooleanNotModel(ValueModel<Boolean> source) {
 		super(source);
@@ -22,4 +23,8 @@ public class BooleanNotModel
 		return Boolean.valueOf(!source.booleanValue());
 	}
 	
+	@Override
+	public MutableBooleanModel asMutable() {
+		return null;
+	}	
 }
