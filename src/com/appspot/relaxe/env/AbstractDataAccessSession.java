6939d705119b4a5eaa762f31e1478194d434f1d2
Topi Nieminen <topi.nieminen@gmail.com>
2013-06-13 16:52:29 +0300

    Add method EnsitySession.newEntity

--- src/com/appspot/relaxe/env/AbstractDataAccessSession.java
+++ src/com/appspot/relaxe/env/AbstractDataAccessSession.java
@@ -311,4 +311,19 @@ public abstract class AbstractDataAccessSession<I extends Implementation<I>>
 	public void flush() {
 		unificationContext.close();
 	}
+	
+
+	@Override
+	public <		
+		A extends Attribute,
+		R extends Reference,
+		T extends ReferenceType<A, R, T, E, H, F, M, ?>,
+		E extends Entity<A, R, T, E, H, F, M, ?>,
+		H extends ReferenceHolder<A, R, T, E, H, M, ?>,
+		F extends EntityFactory<E, H, M, F, ?>,
+		M extends EntityMetaData<A, R, T, E, H, F, M, ?>		
+	>
+	E newEntity(T type) {
+		return type.getMetaData().getFactory().newEntity();
+	}	
 }
--- src/com/appspot/relaxe/service/EntitySession.java
+++ src/com/appspot/relaxe/service/EntitySession.java
@@ -120,5 +120,16 @@ public interface EntitySession {
 	 */
 	void flush();
 	
-	
+
+
+	public <		
+		A extends Attribute,
+		R extends Reference,
+		T extends ReferenceType<A, R, T, E, H, F, M, ?>,
+		E extends Entity<A, R, T, E, H, F, M, ?>,
+		H extends ReferenceHolder<A, R, T, E, H, M, ?>,
+		F extends EntityFactory<E, H, M, F, ?>,
+		M extends EntityMetaData<A, R, T, E, H, F, M, ?>		
+	>
+	E newEntity(T type);
 }
